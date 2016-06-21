d086f44b516e1bca47f1ae8e6614d434b13be694
supermario <supermario>
2016-06-20 23:47:18 -0400

    added vectors parallel Cheeck

--- src/linear_algebra/Vector_Calculations.java
+++ src/linear_algebra/Vector_Calculations.java
@@ -4,6 +4,8 @@ import linear_algebra.Vector.DiffernetDimentionsVectorException;
 
 public class Vector_Calculations {
 
+	public static final double ERROR_MARGIN = 1e-4;
+
 	public Vector addVectors(Vector v1, Vector v2) throws DiffernetDimentionsVectorException, NullPointerException {
 		Vector resultVector;
 		if (v1 != null && v2 != null) {
@@ -67,7 +69,7 @@ public class Vector_Calculations {
 		return Math.sqrt(m);
 	}
 
-	public Vector calculateUnitVector(Vector v) throws IllegalArgumentException {
+	public Vector calculateUnitVector(Vector v, int precision) throws IllegalArgumentException {
 		Vector u = null;
 
 		if (v != null) {
@@ -79,10 +81,26 @@ public class Vector_Calculations {
 		} else {
 			throw new NullPointerException();
 		}
+
 		return u;
 	}
 
 	public double calculateInnerProduct(Vector v1, Vector v2) throws DiffernetDimentionsVectorException {
+		double k = 0.00;
+		if (v1 != null && v2 != null) {
+			if (v1.getCoordinates().length != v2.getCoordinates().length) {
+				throw new DiffernetDimentionsVectorException();
+			}
+			for (int i = 0; i < v1.getCoordinates().length; i++) {
+				k += v1.getCoordinates()[i] * v2.getCoordinates()[i];
+			}
+		} else {
+			throw new NullPointerException();
+		}
+		return k;
+	}
+
+	public double calculateCrossProduct(Vector v1, Vector v2) throws DiffernetDimentionsVectorException {
 		double k = 0.00;
 		if (v1 != null && v2 != null) {
 			if (v1.getCoordinates().length != v2.getCoordinates().length) {
@@ -104,28 +122,87 @@ public class Vector_Calculations {
 		double v2_mag = 0.00;
 		if (v1 != null && v2 != null) {
 			dot = calculateInnerProduct(v1, v2);
-
-	//		System.out.println("dot : " + dot);
-
 			v1_mag = calculateVectorMagnitude(v1);
 			v2_mag = calculateVectorMagnitude(v2);
 
-			double mag_prod = (v1_mag * v2_mag);
-
-		//	System.out.println("mag_prod : " + mag_prod);
-
 			k = dot / (v1_mag * v2_mag);
-
-	//		System.out.println("K : " + k);
-
 			k = Math.acos(k);
-
-	//		System.out.println("K : " + k);
 
 		} else {
 			throw new NullPointerException();
 		}
 		return k;
+	}
+
+	public boolean areVectorsParallel(Vector v1, Vector v2) {
+
+		if (v1 != null && v2 != null) {
+			if (v1.getCoordinates().length != v2.getCoordinates().length) {
+				throw new DiffernetDimentionsVectorException();
+			}
+			if (calculateVectorMagnitude(v1) == 0 || calculateVectorMagnitude(v2) == 0) {
+				return true;
+			}
+			if (checkParallelWithDotProductZeroAngle(v1, v2) == true && checkParallelWithUnitvector(v1, v2) == true) {
+				return true;
+			}
+		} else {
+			throw new NullPointerException();
+		}
+
+		return false;
+	}
+
+	public boolean checkParallelWithDotProductZeroAngle(Vector v1, Vector v2) {
+
+		if (v1 != null && v2 != null) {
+			if (v1.getCoordinates().length != v2.getCoordinates().length) {
+				throw new DiffernetDimentionsVectorException();
+			}
+			double dot_product = calculateInnerProduct(v1, v2);
+			double v1_mag = calculateVectorMagnitude(v1);
+			double v2_mag = calculateVectorMagnitude(v2);
+			if (Math.abs(dot_product) == (v1_mag * v2_mag)) {
+				return true;
+			}
+		} else {
+			throw new NullPointerException();
+		}
+		return false;
+	}
+
+	public boolean checkParallelWithUnitvector(Vector v1, Vector v2) {
+
+		if (v1 != null && v2 != null) {
+			if (v1.getCoordinates().length != v2.getCoordinates().length) {
+				throw new DiffernetDimentionsVectorException();
+			}
+
+			Vector uv1 = calculateUnitVector(v1, 3);
+			Vector uv2 = calculateUnitVector(v2, 3);
+//			System.out.println("U1: " + uv1);
+//			System.out.println("U2: " + uv2);
+//			System.out.println("-U2: " + multiplyVectorsByScalar(uv2, -1));
+
+			Vector difference_vector_1 = subtractVectors(uv1, uv2);
+			Vector difference_vector_2 = subtractVectors(uv1, multiplyVectorsByScalar(uv2, -1));
+
+			for (int i = 0; i < difference_vector_1.getCoordinates().length; i++) {
+				if (Math.abs(difference_vector_1.getCoordinates()[i]) > ERROR_MARGIN
+						&& Math.abs(difference_vector_2.getCoordinates()[i]) > ERROR_MARGIN) {
+					return false;
+				}
+
+			}
+
+		} else {
+			throw new NullPointerException();
+		}
+		return true;
+	}
+
+	public boolean areVectorsOrthogonal(Vector v1, Vector v2) {
+		return false;
 	}
 
 	public void printVector(Vector v) {
@@ -136,65 +213,88 @@ public class Vector_Calculations {
 		// TODO Auto-generated method stub
 		try {
 			Vector_Calculations vc = new Vector_Calculations();
-//			Vector v1 = new Vector(new double[] { 8.218, -9.341 });
-//			Vector v2 = new Vector(new double[] { -1.129, 2.111 });
-//
-//			Vector v3 = new Vector(new double[] { 7.119, 8.215 });
-//			Vector v4 = new Vector(new double[] { -8.223, 0.878 });
-//
-//			Vector v5 = new Vector(new double[] { 1.671, -1.012, -0.318 });
-//			double scalar = 7.41;
-//
-//			// vc.comparePrintVectors(v1, v2);
-//			// vc.comparePrintVectors(v1, v3);
-//			// vc.comparePrintVectors(v1, v4);
-//
-//			vc.addPrintVectors(v1, v2);
-//			System.out.println("");
-//			vc.subtractPrintVectors(v3, v4);
-//			System.out.println("");
-//			vc.multiplyByScalarPrintVectors(v5, scalar);
-//			System.out.println("");
-//
-//			Vector v6 = new Vector(new double[] { -1, 1, 1 });
-//			vc.printVectorMagnitude(v6);
-//			System.out.println("");
-//			vc.printUnitVector(v6);
-//			System.out.println("");
-//			vc.multiplyByScalarPrintVectors(vc.calculateUnitVector(v6), vc.calculateVectorMagnitude(v6));
-//			System.out.println("");
-//
-//			Vector v7 = new Vector(new double[] { -0.221, 7.437 });
-//			Vector v8 = new Vector(new double[] { 8.813, -1.331, -6.247 });
-//			Vector v9 = new Vector(new double[] { 5.581, -2.136 });
-//			Vector v10 = new Vector(new double[] { 1.996, 3.108, -4.554 });
-//
-//			vc.printVectorMagnitude(v7);
-//			System.out.println("");
-//			vc.printVectorMagnitude(v8);
-//			System.out.println("");
-//			vc.printUnitVector(v9);
-//			System.out.println("");
-//			vc.printUnitVector(v10);
-//			System.out.println("");
+			// Vector v1 = new Vector(new double[] { 8.218, -9.341 });
+			// Vector v2 = new Vector(new double[] { -1.129, 2.111 });
+			//
+			// Vector v3 = new Vector(new double[] { 7.119, 8.215 });
+			// Vector v4 = new Vector(new double[] { -8.223, 0.878 });
+			//
+			// Vector v5 = new Vector(new double[] { 1.671, -1.012, -0.318 });
+			// double scalar = 7.41;
+			//
+			// // vc.comparePrintVectors(v1, v2);
+			// // vc.comparePrintVectors(v1, v3);
+			// // vc.comparePrintVectors(v1, v4);
+			//
+			// vc.addPrintVectors(v1, v2);
+			// System.out.println("");
+			// vc.subtractPrintVectors(v3, v4);
+			// System.out.println("");
+			// vc.multiplyByScalarPrintVectors(v5, scalar);
+			// System.out.println("");
+			//
+			// Vector v6 = new Vector(new double[] { -1, 1, 1 });
+			// vc.printVectorMagnitude(v6);
+			// System.out.println("");
+			// vc.printUnitVector(v6);
+			// System.out.println("");
+			// vc.multiplyByScalarPrintVectors(vc.calculateUnitVector(v6),
+			// vc.calculateVectorMagnitude(v6));
+			// System.out.println("");
+			//
+			// Vector v7 = new Vector(new double[] { -0.221, 7.437 });
+			// Vector v8 = new Vector(new double[] { 8.813, -1.331, -6.247 });
+			// Vector v9 = new Vector(new double[] { 1, 0 });
+			// Vector v10 = new Vector(new double[] { 1.996, 3.108, -4.554 });
+			//
+			// vc.printVectorMagnitude(v7);
+			// System.out.println("");
+			// vc.printVectorMagnitude(v8);
+			// System.out.println("");
+			// vc.printUnitVector(v8);
+			// System.out.println("");
+			// vc.printUnitVector(v10);
+			// System.out.println("");
+			//
+			// Vector v11 = new Vector(new double[] { 7.887, 4.138 });
+			// Vector v12 = new Vector(new double[] { -8.802, 6.776 });
+			// Vector v13 = new Vector(new double[] { -5.955, -4.904, -1.874 });
+			// Vector v14 = new Vector(new double[] { -4.496, -8.755, 7.103 });
+			// Vector v15 = new Vector(new double[] { 3.183, -7.627 });
+			// Vector v16 = new Vector(new double[] { -2.668, 5.319 });
+			// Vector v17 = new Vector(new double[] { 7.35, 0.221, 5.188 });
+			// Vector v18 = new Vector(new double[] { 2.751, 8.259, 3.985 });
+			//
+			// vc.printVectorInnerProduct(v11, v12);
+			// System.out.println("");
+			// vc.printVectorInnerProduct(v13, v14);
+			// System.out.println("");
+			//
+			// vc.printVectorsAngleInRadians(v15, v16);
+			// System.out.println("");
+			// vc.printVectorsAngleInDegrees(v17, v18);
+			// System.out.println("");
 
-			Vector v11 = new Vector(new double[] { 7.887, 4.138 });
-			Vector v12 = new Vector(new double[] { -8.802, 6.776 });
-			Vector v13 = new Vector(new double[] { -5.955, -4.904, -1.874 });
-			Vector v14 = new Vector(new double[] { -4.496, -8.755, 7.103 });
-			Vector v15 = new Vector(new double[] { 3.183, -7.627 });
-			Vector v16 = new Vector(new double[] { -2.668, 5.319 });
-			Vector v17 = new Vector(new double[] { 7.35, 0.221, 5.188 });
-			Vector v18 = new Vector(new double[] { 2.751, 8.259, 3.985 });
+			Vector v1 = new Vector(new double[] { -7.579, -7.88 });
+			Vector v2 = new Vector(new double[] { 22.737, 23.64 });
+			Vector v3 = new Vector(new double[] { -2.029, 9.97, 4.172 });
+			Vector v4 = new Vector(new double[] { -9.231, -6639, -7.245 });
+			Vector v5 = new Vector(new double[] { -2.328, -7.284, -1.214 });
+			Vector v6 = new Vector(new double[] { -1.821, 1.072, -2.94 });
+			Vector v7 = new Vector(new double[] { 2.118, 4.827 });
+			Vector v8 = new Vector(new double[] { 0, 0 });
 
-			vc.printVectorInnerProduct(v11, v12);
+			vc.printVectorsAngleInDegrees(v1, v2);
+			vc.printVectorsParallelnesCheck(v1, v2);
 			System.out.println("");
-			vc.printVectorInnerProduct(v13, v14);
+			vc.printVectorsAngleInDegrees(v3, v4);
+			vc.printVectorsParallelnesCheck(v3, v4);
 			System.out.println("");
-			
-			vc.printVectorsAngleInRadians(v15, v16);
+			vc.printVectorsAngleInDegrees(v5, v6);
+			vc.printVectorsParallelnesCheck(v5, v6);
 			System.out.println("");
-			vc.printVectorsAngleInDegrees(v17, v18);
+			vc.printVectorsAngleInDegrees(v7, v8);
+			vc.printVectorsParallelnesCheck(v7, v8);
 			System.out.println("");
 
 		} catch (DiffernetDimentionsVectorException e) {
@@ -225,7 +325,7 @@ public class Vector_Calculations {
 	}
 
 	public void printUnitVector(Vector v) {
-		System.out.println("Vector  :" + v.toString() + " -->  Unit vector: " + calculateUnitVector(v));
+		System.out.println("Vector  :" + v.toString() + " -->  Unit vector: " + calculateUnitVector(v, 3));
 	}
 
 	public void printVectorInnerProduct(Vector v1, Vector v2) {
@@ -246,6 +346,11 @@ public class Vector_Calculations {
 	public void printVectorsAngleInDegrees(Vector v1, Vector v2) {
 		System.out.println("Vectors angle deg:" + v1.toString() + " + " + v2.toString() + "  =  "
 				+ Math.toDegrees(calculateVectorsAngle(v1, v2)));
+	}
+
+	public void printVectorsParallelnesCheck(Vector v1, Vector v2) {
+		System.out.println(
+				"Vectors parallel : " + v1.toString() + " + " + v2.toString() + "  ?  " + areVectorsParallel(v1, v2));
 	}
 
 }
